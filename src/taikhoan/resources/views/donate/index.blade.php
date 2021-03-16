2ecc5b24b576dfe2a9b83e032ac947166d147aad
Vu Developer <vubach2912@gmail.com>
2021-03-16 22:08:19 +0700

    Update

--- src/taikhoan/resources/views/donate/index.blade.php
+++ src/taikhoan/resources/views/donate/index.blade.php
@@ -14,14 +14,42 @@
                 <div class="block-content">
                     <div class="ntt-receive-info">
                         <div class="tab-content ntt-receive-content col-65">
+                            <div id="bank" class="tab-pane">
+
+                                <div><strong>TECHCOMBANK</strong></div>
+                                <ul>
+                                    <li>Vui lòng chọn hình thức: <strong>Ck nhanh 24/7</strong></li>
+                                    <li>Tên chủ tài khoản: <strong>{{ env('DONATE_CK_TECH_NAME', 'Phạm Việt An')}}</strong></li>
+                                    <li>Số tài khoản: <strong>{{ env('DONATE_CK_TECH_NUMBER', '19024143666686')}}</strong> &nbsp;
+                                        <a class="{{ env('DONATE_CK_TECH_NUMBER', '19024143666686')}}"
+                                           onclick="copyTK('modal_ntt_confirm', '{{ env('DONATE_CK_TECH_NUMBER', '19024143666686')}}');"
+                                           style="background-color: #00a9ff;text-align: center;color: #fff;padding: 3px 10px;border-radius: 10px;">Copy</a>
+                                    </li>
+                                    <li>Nội dung: <strong class="cuphap"> {{ env('DONATE_STRING', 'hk')}} {{ Auth::user()->name}}</strong></li>
+                                </ul>
+
+                                <div><strong>VIETCOMBANK</strong></div>
+                                <ul>
+                                    <li>Vui lòng chọn hình thức: <strong>Ck nhanh 24/7</strong></li>
+                                    <li>Tên chủ tài khoản: <strong>{{ env('DONATE_CK_VCB_NAME', 'Phạm Việt An')}}</strong></li>
+                                    <li>Số tài khoản: <strong>{{ env('DONATE_CK_VCB_NUMBER', '0541000271925')}}</strong> &nbsp;
+                                        <a class="{{ env('DONATE_CK_VCB_NUMBER', '0541000271925')}}"
+                                           onclick="copyTK('modal_ntt_confirm', '{{ env('DONATE_CK_VCB_NUMBER', '0541000271925')}}');"
+                                           style="background-color: #00a9ff;text-align: center;color: #fff;padding: 3px 10px;border-radius: 10px;">Copy</a>
+                                    </li>
+                                    <li>Nội dung: <strong class="cuphap"> {{ env('DONATE_STRING', 'hk')}} {{ Auth::user()->name}}</strong></li>
+                                </ul>
+
+                            </div>
                             <div id="momo" class="tab-pane active">
                                 <ul>
-                                    <li>Tài khoản: <strong>0378435791 (Đỗ Hằng Nga)</strong> &nbsp; <a class="0378435791"
+                                    <li>Tài khoản: <strong>{{ env('MOMO_NUMBER', '0378435791')}} ({{ env('MOMO_NAME', 'Đỗ Hằng Nga')}})
+                                        </strong> &nbsp; <a class="{{ env('MOMO_NUMBER', '0378435791')}}"
                                             onclick="copyTK('ntt-receive-info-header-pc', '0964918575');"
                                             style="background-color: #00a9ff;text-align: center;color: #fff;padding: 3px 10px;border-radius: 10px;">Copy</a>
                                     </li>
                                     <li>Số tiền: (số tiền mà bạn muốn donate)</li>
-                                    <li>Nội dung: <strong class="cuphap">ktts-{{ Auth::user()->name}}</strong></li>
+                                    <li>Nội dung: <strong class="cuphap">{{ env('DONATE_STRING', 'hk')}} {{ Auth::user()->name}}</strong></li>
                                 </ul>
                             </div>
                         </div>
@@ -29,7 +57,7 @@
                         <ul class="nav nav-pills flex-column ntt-receive-tabs col-35" role="tablist">
                             <li class="nav-item active" onclick="openBank(event, 'momo')">
                                 <a aria-controls="momo" role="tab" data-toggle="tab" aria-expanded="true">
-                                    <img src="frontend/img/momo.png" class="img-responsive"> <span>Momo</span>
+                                    <img src="taikhoan/frontend/img/momo.png" class="img-responsive"> <span>Momo</span>
                                 </a>
                             </li>
                         </ul>
@@ -45,11 +73,6 @@
                 <div class="block-content">
                     <div class="ntt-receive-info">
                         <div class="ntt-receive-content col-65">
-{{--                            <div class="form-group">--}}
-{{--                                <label>Số điện thoại liên hệ (<font color="red">*</font>)</label>--}}
-{{--                                <input type="tel" name="player_phone" placeholder="Nhập Số điện thoại" class="form-control"--}}
-{{--                                    value="" onkeyup="user_update_cuphap(this)" autocomplete="none">--}}
-{{--                            </div>--}}
                             <div class="message error hide" style="color:red; margin-bottom:15px;" id="cuphap-msg-error">
                             </div>
 
@@ -57,7 +80,7 @@
                                 <label>Nội dung chuyển khoản</label>
                                 <div class="input-group">
                                     <input type="text" class="form-control" id="cuphapck_ntt_game" readonly=""
-                                        value="ktts-{{$username}}">
+                                        value="{{ env('DONATE_STRING', 'hk')}} {{$username}}">
                                     <span class="input-group-btn">
                                         <button class="btn btn-primary" type="button"
                                             onclick="copy_cuphap_ck('ntt-receive-info-header')"
@@ -81,15 +104,6 @@
                                     toán</button>
                             </div>
                         </div>
-                        <div class="ntt-receive-qrcode col-35 hidden-xs">
-                            <p style="margin-top:0">Hoặc quét QRCODE MOMO</p>
-                            <p>
-                            </p>
-                            <div class="ntt-qrcode" id="ntt-qrcode"><img
-                                    width="200px"
-                                    src="./frontend/img/qrcode.png"
-                                    class="img-responsive"></div>
-                        </div>
                     </div>
                 </div>
             </div>
@@ -106,39 +120,53 @@
 
                             <div class="popup-transfer">
                                 <ul class="nav nav-tabs" role="tablist">
-{{--                                    <li role="presentation" class="nav-item active" onclick="openBank(event, 'bank_mob')">--}}
-{{--                                        <a href="#bank_mob" aria-controls="bank" role="tab" data-toggle="tab">--}}
-{{--                                            <img src="frontend/img/popup_bank.png" class="img-responsive"> <span>Ngân hàng</span>--}}
-{{--                                        </a>--}}
-{{--                                    </li>--}}
+                                    <li role="presentation" class="nav-item active" onclick="openBank(event, 'bank_mob')">
+                                        <a href="#bank_mob" aria-controls="bank" role="tab" data-toggle="tab">
+                                            <img src="taikhoan/frontend/img/popup_bank.png" class="img-responsive"> <span>Ngân hàng</span>
+                                        </a>
+                                    </li>
                                     <li role="presentation" class="nav-item active" onclick="openBank(event, 'momo_mob')">
                                         <a href="#momo_mob" aria-controls="momo" role="tab" data-toggle="tab">
-                                            <img src="frontend/img/momo.png" class="img-responsive"> <span>Momo</span>
+                                            <img src="taikhoan/frontend/img/momo.png" class="img-responsive"> <span>Momo</span>
                                         </a>
                                     </li>
                                 </ul>
 
                                 <div class="tab-content">
-{{--                                    <div id="bank_mob" class="tab-pane active">--}}
-{{--                                        <div><strong>VP BANK - Ngân Hàng TMCP Việt Nam Thịnh Vượng</strong></div>--}}
-{{--                                        <ul>--}}
-{{--                                            <li>Vui lòng chọn hình thức: <strong>Ck nhanh 24/7</strong></li>--}}
-{{--                                            <li>Tên chủ tài khoản: <strong>GAMOTA CORP</strong></li>--}}
-{{--                                            <li>Số tài khoản: <strong>206402668</strong> &nbsp; <a class="206402668"--}}
-{{--                                                    onclick="copyTK('modal_ntt_confirm', '206402668');"--}}
-{{--                                                    style="background-color: #00a9ff;text-align: center;color: #fff;padding: 3px 10px;border-radius: 10px;">Copy</a>--}}
-{{--                                            </li>--}}
-{{--                                            <li>Chi nhánh: <strong>BA ĐÌNH - Hà Nội</strong></li>--}}
-{{--                                            <li>Nội dung: <strong class="cuphap"> annguyen323010</strong></li>--}}
-{{--                                        </ul>--}}
-{{--                                    </div>--}}
+                                    <div id="bank_mob" class="tab-pane">
+
+                                        <div><strong>TECHCOMBANK</strong></div>
+                                        <ul>
+                                            <li>Vui lòng chọn hình thức: <strong>Ck nhanh 24/7</strong></li>
+                                            <li>Tên chủ tài khoản: <strong>{{ env('DONATE_CK_TECH_NAME', 'Phạm Việt An')}}</strong></li>
+                                            <li>Số tài khoản: <strong>{{ env('DONATE_CK_TECH_NUMBER', '19024143666686')}}</strong> &nbsp;
+                                                <a class="{{ env('DONATE_CK_TECH_NUMBER', '19024143666686')}}"
+                                                   onclick="copyTK('modal_ntt_confirm', '{{ env('DONATE_CK_TECH_NUMBER', '19024143666686')}}');"
+                                                   style="background-color: #00a9ff;text-align: center;color: #fff;padding: 3px 10px;border-radius: 10px;">Copy</a>
+                                            </li>
+                                            <li>Nội dung: <strong class="cuphap"> {{ env('DONATE_STRING', 'hk')}} {{ Auth::user()->name}}</strong></li>
+                                        </ul>
+
+                                        <div><strong>VIETCOMBANK</strong></div>
+                                        <ul>
+                                            <li>Vui lòng chọn hình thức: <strong>Ck nhanh 24/7</strong></li>
+                                            <li>Tên chủ tài khoản: <strong>{{ env('DONATE_CK_VCB_NAME', 'Phạm Việt An')}}</strong></li>
+                                            <li>Số tài khoản: <strong>{{ env('DONATE_CK_VCB_NUMBER', '0541000271925')}}</strong> &nbsp;
+                                                <a class="{{ env('DONATE_CK_VCB_NUMBER', '0541000271925')}}"
+                                                   onclick="copyTK('modal_ntt_confirm', '{{ env('DONATE_CK_VCB_NUMBER', '0541000271925')}}');"
+                                                   style="background-color: #00a9ff;text-align: center;color: #fff;padding: 3px 10px;border-radius: 10px;">Copy</a>
+                                            </li>
+                                            <li>Nội dung: <strong class="cuphap"> {{ env('DONATE_STRING', 'hk')}} {{ Auth::user()->name}}</strong></li>
+                                        </ul>
+
+                                    </div>
                                     <div id="momo_mob" class="tab-pane active">
                                         <ul>
-                                            <li>Tài khoản: <strong>0378435791 (Đỗ Hằng Nga)</strong> &nbsp; <a
-                                                    class="0378435791" onclick="copyTK('modal_ntt_confirm', '0378435791');"
+                                            <li>Tài khoản: <strong>{{ env('MOMO_NUMBER', '0378435791')}} ({{ env('MOMO_NAME', 'Đỗ Hằng Nga')}})</strong> &nbsp; <a
+                                                    class="{{ env('MOMO_NUMBER', '0378435791')}}" onclick="copyTK('modal_ntt_confirm', '{{ env('MOMO_NUMBER', '0378435791')}}');"
                                                     style="background-color: #00a9ff;text-align: center;color: #fff;padding: 3px 10px;border-radius: 10px;">Copy</a>
                                             </li>
-                                            <li>Nội dung: <strong class="cuphap">ktts-{{ Auth::user()->name}}</strong></li>
+                                            <li>Nội dung: <strong class="cuphap">{{ env('DONATE_STRING', 'hk')}} {{ Auth::user()->name}}</strong></li>
                                         </ul>
                                     </div>
                                 </div>
